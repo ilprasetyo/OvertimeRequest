b578196bdefc3fc4a08429cde3dd94284ed5dc5b
Ilham Prasetyo <Ilprasetyo2125@gmail.com>
2021-05-25 14:34:55 +0700

    First Commit Start Project

---
+++ OvertimeRequest/OvertimeRequestMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OvertimeRequestMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
