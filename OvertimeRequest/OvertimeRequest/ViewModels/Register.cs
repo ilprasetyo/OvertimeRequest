5496c4cb253d5c515206565aa68f037d5557a10c
Ilham Prasetyo <Ilprasetyo2125@gmail.com>
2021-05-27 19:20:41 +0700

    Fix ViewModels Register

--- OvertimeRequest/OvertimeRequest/ViewModels/Register.cs
+++ OvertimeRequest/OvertimeRequest/ViewModels/Register.cs
@@ -15,12 +15,7 @@ namespace OvertimeRequest.ViewModels
         public string Gender { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
-        public StatusRequest Status { get; set; }
-        
-        public enum StatusRequest
-        {
-            Waiting,
-            Approve,
-            Reject
-        }
+        public string Status { get; set; }
+        public int Position { get; set; }
+    }
 }
