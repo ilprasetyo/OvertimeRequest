957cbdab73b32ff98b546226f0d99b7484112906
Anindya <pangestianin@gmail.com>
2021-05-31 16:32:17 +0700

    update Request Controller and Send Email

--- OvertimeRequest/OvertimeRequest/Controllers/RequestController.cs
+++ OvertimeRequest/OvertimeRequest/Controllers/RequestController.cs
@@ -1,26 +1,65 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Dapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using OvertimeRequest.Base;
+using OvertimeRequest.Context;
+using OvertimeRequest.Handlers;
 using OvertimeRequest.Models;
 using OvertimeRequest.Repositories.Data;
+using OvertimeRequest.Repositories.Interface;
+using OvertimeRequest.Services;
+using OvertimeRequest.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OvertimeRequest.Controllers
 {
     [Route("api/[controller]")]
-    [Authorize]
+   // [Authorize]
     [ApiController]
     public class RequestController : BaseController<Request, RequestRepository, int>
     {
         private RequestRepository requestRepository;
+        private MyContext myContext;
+        private readonly IConfiguration _configuration;
+        private readonly IGenericDapper _dapper;
 
-        public RequestController(RequestRepository requestRepository) : base(requestRepository)
+        public RequestController(RequestRepository requestRepository, MyContext myContext, IConfiguration configuration, IGenericDapper dapper) : base(requestRepository)
         {
             this.requestRepository = requestRepository;
+            this.myContext = myContext;
+            _configuration = configuration;
+            _dapper = dapper;
         }
+
+        [HttpPost("RequestEmployee")]
+        public ActionResult RequestEmployee(RequestVM requestVM)
+        {
+           
+            var getEmployee = myContext.Employees.Where(e => e.NIK == requestVM.NIK).FirstOrDefault();
+
+           
+                var dbprams = new DynamicParameters();
+                dbprams.Add("NIK", requestVM.NIK, DbType.String);
+                dbprams.Add("StartHours", requestVM.StartHours, DbType.DateTime);
+                dbprams.Add("EndHours", requestVM.EndHours, DbType.DateTime);
+                dbprams.Add("Reason", requestVM.Reason, DbType.String);
+
+                var result = Task.FromResult(_dapper.Insert<int>("[dbo].[SP_OvertimeRequest]"
+                    , dbprams,
+                    commandType: CommandType.StoredProcedure));
+                //send email
+                var sendEmail = new SendEmail(myContext);
+                sendEmail.SendEmailSubmitRequest(getEmployee);
+            
+                return Ok(new { Status = "Success", Message = "Request Has Been Sent, Check your email" });
+        }
+
+
     }
 }
--- OvertimeRequest/OvertimeRequest/ViewModels/RequestVM.cs
+++ OvertimeRequest/OvertimeRequest/ViewModels/RequestVM.cs
@@ -5,19 +5,12 @@ using System.Threading.Tasks;
 
 namespace OvertimeRequest.ViewModels
 {
-    public class Request
+    public class RequestVM
     {
+        public string NIK { get; set; }
         public DateTime StartHours { get; set; }
         public DateTime EndHours { get; set; }
         public string Reason { get; set; }
-        public StatusRequest Status { get; set; }
-        public int Payroll { get; set; }
-        public int Quota { get; set; }
-        public enum StatusRequest
-        {
-            Waiting,
-            Approve,
-            Reject
-        }
+  
     }
 }
